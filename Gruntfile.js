58a1a1d9ccef0e1a757c0545f353f757134c5fe0
Carrie Forde <carrie@Carries-MacBook.local>
2018-01-18 20:47:28 -0800

    Initial commit :tada:

---
+++ Gruntfile.js
@@ -0,0 +1,151 @@
+module.exports = function (grunt) {
+
+  // Load all packages.
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('postcss-import')({
+            from: 'assets/css/index.css',
+            result: 'main.css'
+          }),
+          require('autoprefixer')({ browsers: 'last 2 versions' }), // Add vendor prefixes.
+          require('css-mqpacker')({ sort: true }),
+          // require('cssnano')( { preset: 'default' })
+        ]
+      },
+      dist: {
+        src: 'assets/css/*.css',
+        dest: 'main.css'
+      }
+    },
+    babel: {
+      options: {
+        sourceMap: false
+      },
+      dist: {
+        files: {
+          'app.js': 'app.js'
+        }
+      }
+    },
+    concat: {
+      options: {
+        banner:
+          '/*! <%= pkg.name %> JS - This file is built with Grunt. DO NOT EDIT! */\n\n',
+        separator: '\n\n',
+        sourceMap: true
+      },
+      dist: {
+        src: ['assets/scripts/*.js'],
+        dest: 'assets/app.js'
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'app.min.js': 'assets/app.js'
+        }
+      }
+    },
+    stylelint: {
+      options: {
+        configFile: '.stylelintrc',
+        failOnError: true,
+        syntax: 'scss'
+      },
+      src: ['assets/sass/**/*.scss']
+    },
+    eslint: {
+      options: {
+        configFile: '.eslintrc.js'
+      },
+      target: ['assets/scripts/*.js']
+    },
+    imagemin: {
+      dynamic: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/',
+            src: ['images/*.{png,jpg,gif}'],
+            dest: 'dist/images',
+            flatten: true
+          }
+        ]
+      }
+    },
+    watch: {
+      css: {
+        files: ['assets/css/*.css'],
+        tasks: ['styles']
+      },
+      js: {
+        files: ['assets/scripts/*.js'],
+        tasks: ['scripts']
+      },
+      sprites: {
+        files: ['assets/icons/*.svg'],
+        tasks: ['svgmin', 'svgstore']
+      }
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: ['*.css', 'assets/scripts/*.js', '*.html']
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: './'
+          },
+          injectChanges: true
+        }
+      }
+    },
+    svgmin: {
+      options: {
+        plugins: [{ removeViewBox: false }]
+      },
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/',
+            src: ['icons/*.svg'],
+            dest: 'assets/icons',
+            flatten: true
+          }
+        ]
+      }
+    },
+    svgstore: {
+      dist: {
+        files: {
+          'assets/icons/svg-defs.svg': ['assets/icons/svg/*.svg']
+        }
+      },
+      options: {
+        cleanup: true
+      }
+    }
+  });
+
+  // Configure tasks.
+  grunt.registerTask('default', ['browserSync', 'watch']);
+
+  grunt.registerTask('styles', ['stylelint', 'postcss']);
+
+  grunt.registerTask('scripts', ['concat', 'eslint', 'babel']);
+
+  grunt.registerTask('icons', ['svgmin', 'svgstore']);
+
+  grunt.registerTask('lint', ['stylelint', 'eslint']);
+
+  grunt.registerTask('build', ['styles', 'icons', 'minify:full']);
+};
--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# ai-humanity-hackathon
+# AI Humanity Hackathon 
+
+## Getting started
+To get started, clone this repo to your computer:
+
+```sh
+git clone https://github.com/Jing-Ran/ai-humanity-hackathon.git /path/to/project/
+```
+Once you have cloned the project, move into the project directory and install all the Node dependencies:
+
+```sh
+cd /path/to/project/
+npm install
+```
+
+And since this project is configured with BrowserSync, it's highly recommended to that globally:
+
+```sh
+npm install -g browser-sync
+```
+
+This will inject CSS changes automatically, and refresh the browser when changes are made to markup and JavaScript files. :smile:
+
+## Project structure
+```
+|-- assets/
+|--|-- css/
+|--|--|-- individual files go here
+|--|-- images/
+|--|-- js/
+|--|--|-- individual files go here
+|-- index.html
+|-- main.css (this would be the file that imports all other files)
+|-- app.js
+|-- .editorconfig
+|-- .gitignore
+|-- .editorconfig
+|-- .eslintrc
+|-- .prettier
+|-- .stylelintrc
+```
+
+## Tasks
+Once you have run `npm install`, you're ready to start using this repo's taskrunning features. The project includes several grunt tasks.
+
+### `grunt`
+This default task kicks off BrowserSync and `grunt watch`.
+
+### `grunt watch`
+On file save:
+- Lints, compiles, and runs PostCSS on styles
+- Concatenates, transpiles, and minifies JavaScript files
+- Minifies and concatenates SVG files
+- This task does not use BrowserSync. Use the default grunt task instead.
+
+### `grunt styles`
+This tasks lints files using Stylelint, compiles the Sass, This task lints files using Stylelint, which outputs warnings and errors to the console, compiles the Sass into src/style.css, and processes style.css with PostCSS (autoprefixing and Media Query sorting).
+
+### `grunt scripts`
+This task concatenates all scripts in src/assets/scripts, transpiles any ES2015 (ES6) or newer JavaScript through Babel, and lints files agains the .eslintrc.js.
+
+### `grunt icons`
+Minify individual SVG icon files and concatenate the SVG into a single SVG sprite.
+
+### `grunt lint`
+Lints Sass and preprocessed JavaScript files against Stylelint and ES Lint, respectively.
+
+### `grunt minify & grunt minify:true`
+Minifies styles, scripts, and icons, outputting to the root src directory. Passing true to the task will also minify images.
+
+### `grunt build`
+This tasks runs styles, scripts, icons, and minify:full. It's meant primarily as a step prior to pushing to `development`.
