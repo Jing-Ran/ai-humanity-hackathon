782734af16e772d01b2cf3b5ffb4d6f864eda3bf
Carrie Forde <carrie@carrieforde.com>
2018-01-19 10:59:22 -0800

    Adds accordion js file

---
+++ assets/js/accordion.js
@@ -0,0 +1,11 @@
+/**
+ * Sets up accordion functionality.
+ * 
+ * @class Accordion
+ */
+class Accordion { // eslint-disable-line no-unused-vars
+
+  constructor(target) {
+    this.target = target;
+  }
+}
